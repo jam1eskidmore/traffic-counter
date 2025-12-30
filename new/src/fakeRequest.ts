57381475421ac059219680220e4df8888db0fc38
Jamie Skidmore <jskidmore@lush.com>
2025-12-29 18:23:43 -0800

    implement zod to validate and transform data received from sensor

--- new/src/fakeRequest.ts
+++ new/src/fakeRequest.ts
@@ -1,69 +1,56 @@
 export const generateFakeSensorData = () => {
-  const getRandomTimeValue = (max: number): string => {
-    const value = Math.floor(Math.random() * max);
-    return value < 10 ? `0${value}` : value.toString();
-  };
+	const getRandomTimeValue = (max: number): string => {
+		const value = Math.floor(Math.random() * max);
+		return value < 10 ? `0${value}` : value.toString();
+	};
 
-  const generateCountArray = () => {
-    const numberOfEntries = Math.floor(Math.random() * 10);
-    const countArray = [];
+	const generateCountArray = () => {
+		const numberOfEntries = Math.floor(Math.random() * 10);
+		const countArray = [];
 
-    for (let i = 0; i <= numberOfEntries; i++) {
-      const randomEntersValue = Math.floor(Math.random() * 10);
-      const randomExitsValue = Math.floor(Math.random() * 10);
-      const randomStartHour = getRandomTimeValue(24);
-      const randomStartMinute = getRandomTimeValue(59);
-      let endMinuteHelper = parseInt(randomStartMinute);
-      endMinuteHelper++;
-      let endMinute;
-      endMinuteHelper < 10
-        ? (endMinute = `0${endMinuteHelper}`)
-        : (endMinute = endMinuteHelper.toString());
+		for (let i = 0; i <= numberOfEntries; i++) {
+			const randomEntersValue = Math.floor(Math.random() * 10);
+			const randomExitsValue = Math.floor(Math.random() * 10);
+			const randomStartHour = getRandomTimeValue(24);
+			const randomStartMinute = getRandomTimeValue(59);
+			let endMinuteHelper = parseInt(randomStartMinute);
+			endMinuteHelper++;
+			let endMinute;
+			endMinuteHelper < 10 ? (endMinute = `0${endMinuteHelper}`) : (endMinute = endMinuteHelper.toString());
 
-      countArray.push(`
-                <count>
-                    <startTime>${randomStartHour}:${randomStartMinute}:00</startTime>
-                    <endTime>${randomStartHour}:${endMinute}:00</endTime>
-                    <enters>${randomEntersValue}</enters>
-                    <exits>${randomExitsValue}</exits>
-                    <status>0</status>
-                </count>
-            `);
-    }
-    return countArray.join("");
-  };
+			countArray.push(`
+        <Count StartTime="${randomStartHour}:${randomStartMinute}:00" EndTime ="${randomStartHour}:${endMinute}:00" UnixStartTime="0" Enters="${randomEntersValue}" Exits="${randomExitsValue}" Status="0"/>
+        `);
+		}
+		return countArray.join('');
+	};
 
-  return `
+	return `
     <?xml version="1.0" encoding="UTF-8"?>
-    <metrics>
-      <siteId>665</siteId>
-      <siteName>International Plaza (665)</siteName>
-      <deviceId>SD-01</deviceId>
-      <deviceName>Entrance</deviceName>
-      <properties>
-        <macAddress>00:b0:9d:c8:63:96</macAddress>
-        <ipAddress>10.10.81.100</ipAddress>
-        <hostName>Cam-13132694</hostName>
-        <hwPlatform>2300</hwPlatform>
-        <timezone>-5</timezone>
-        <dst>1</dst>
-        <deviceType>0</deviceType>
-        <serialNumber>13132694</serialNumber>
-      </properties>
-      <reportData>
-        <interval>1</interval>
-        <report>
-          <date>2025-11-18</date>
-          <object>
-            <id>0</id>
-            <deviceId>SD-01</deviceId>
-            <deviceName>Entrance</deviceName>
-            <objectType>0</objectType>
-            <name>Entrance</name>
+    <Metrics SiteId="665" Sitename="International Plaza (665)" DeviceId="SD-01" Devicename="Entrance">
+      <Properties>
+        <Version>2</Version>
+        <TransmitTime>1763760645</TransmitTime>
+        <MacAddress>00:b0:9d:c8:63:96</MacAddress>
+        <IpAddress>10.10.81.100</IpAddress>
+        <HostName>Cam-13132694</HostName>
+        <HttpPort>80</HttpPort>
+        <HttpsPort>443</HttpsPort>
+        <Timezone>-5</Timezone>
+        <TimezoneName>-5</TimezoneName>
+        <DST>1</DST>
+        <HwPlatform>2300</HwPlatform>
+        <SerialNumber>13132694</SerialNumber>
+        <DeviceType>0</DeviceType>
+        <SwRelease>5.6.18.554</SwRelease>
+      </Properties>
+      <ReportData Interval="1">
+        <Report Date="2025-11-18">
+          <Object Id="0" DeviceId="SD-01" DeviceName="Entrance" ObjectType="0" Name="Door 01">
             ${generateCountArray()}
-          </object>
-        </report>
-      </reportData>
-    </metrics>
+          </Object>
+        </Report>
+      </ReportData>
+    </Metrics>
   `.trim();
 };
--- new/src/index.ts
+++ new/src/index.ts
@@ -1,36 +1,116 @@
 import { fakeDataDirect, writeToFakeDb, type DataDirectRow } from './fakeDb';
 import { generateFakeSensorData } from './fakeRequest';
 import { parseStringPromise } from 'xml2js';
+import * as z from 'zod';
+
+// need to come up with solution for store ID
+
+const CountSchema = z.object({
+	$: z.object({
+		StartTime: z.string(),
+		// EndTime: z.string(),
+		// UnixStartTime: z.string(),
+		Enters: z.coerce.number(),
+		Exits: z.coerce.number(),
+		// Status: z.string(),
+	}),
+});
+
+const CountArraySchema = z
+	.union([CountSchema, z.array(CountSchema)])
+	.transform((countData) => (Array.isArray(countData) ? countData : [countData]));
+
+const TrafficDataSchema = z.object({
+	Metrics: z.object({
+		$: z.object({
+			SiteId: z.string(),
+			// Sitename: z.string(),
+			// DeviceId: z.string(),
+			// Devicename: z.string(),
+		}),
+		// Properties: z.object({
+		// 	Version: z.string(),
+		// 	TransmitTime: z.string(),
+		// 	MacAddress: z.string(),
+		// 	IpAddress: z.string(),
+		// 	HostName: z.string(),
+		// 	HttpPort: z.string(),
+		// 	HttpsPort: z.string(),
+		// 	Timezone: z.string(),
+		// 	TimezoneName: z.string(),
+		// 	DST: z.string(),
+		// 	HwPlatform: z.string(),
+		// 	SerialNumber: z.string(),
+		// 	DeviceType: z.string(),
+		// 	SwRelease: z.string(),
+		// }),
+		ReportData: z.object({
+			// $: z.object({ Interval: z.string() }),
+			Report: z.object({
+				$: z.object({ Date: z.string() }),
+				Object: z.object({
+					// $: z.object({
+					// 	Id: z.string(),
+					// 	DeviceId: z.string(),
+					// 	DeviceName: z.string(),
+					// 	ObjectType: z.string(),
+					// 	Name: z.string(),
+					// }),
+					Count: CountArraySchema,
+				}),
+			}),
+		}),
+	}),
+});
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		const xmlContent = generateFakeSensorData();
+		const xmlTrafficData = generateFakeSensorData();
 
-		const parsedXml = await parseStringPromise(xmlContent);
+		const jsonTrafficData = await parseStringPromise(xmlTrafficData, { explicitArray: false });
 
-		const storeId = parsedXml.metrics.siteId[0];
-		storeId.replace(' LUSH', '');
-		const storeDate = parsedXml.metrics.reportData[0].report[0].date[0];
+		// insert logging step here
+		console.log('start');
 
-		const countData = parsedXml.metrics.reportData[0].report[0].object[0].count;
-		const insertedData: DataDirectRow[] = [];
+		const validatedTrafficData = TrafficDataSchema.safeParse(jsonTrafficData);
+		console.log(validatedTrafficData.data?.Metrics.ReportData);
+		console.log('-------------------------------');
+		console.log(validatedTrafficData.data?.Metrics.ReportData.Report);
+		console.log('-------------------------------');
 
-		countData.forEach((e: any) => {
-			const dataToInsert = {
-				storeId: +storeId,
-				entrances: +e.enters,
-				exits: +e.exits,
-				whenstamp: `${storeDate} ${e.startTime}`,
-			} satisfies DataDirectRow;
+		console.log(validatedTrafficData.data?.Metrics.ReportData.Report.Object);
+		console.log('done');
 
-			writeToFakeDb(dataToInsert);
-			insertedData.push(dataToInsert);
-		});
+		if (validatedTrafficData.data) {
+			const storeId = validatedTrafficData.data.Metrics.$.SiteId;
+			storeId.replace(' LUSH', '');
 
-		return new Response(
-			`Data saved!\n\nThe following new data was added to the fakeDb:\n${JSON.stringify(insertedData)}\n\nFull data:\n${JSON.stringify(
-				fakeDataDirect
-			)}`
-		);
+			const storeDate = validatedTrafficData.data.Metrics.ReportData.Report.$.Date;
+
+			// const insertedData: DataDirectRow[] = [];
+			const count = validatedTrafficData.data.Metrics.ReportData.Report.Object.Count;
+
+			let insertedData: DataDirectRow[] = [];
+
+			count.forEach((e: any) => {
+				const dataToInsert = {
+					storeId: +storeId,
+					entrances: e.$.Enters,
+					exits: e.$.Exits,
+					whenstamp: `${storeDate} ${e.$.StartTime}`,
+				} satisfies DataDirectRow;
+
+				writeToFakeDb(dataToInsert);
+				insertedData.push(dataToInsert);
+			});
+
+			return new Response(
+				`Data saved!\n\nThe following new data was added to the fakeDb:\n${JSON.stringify(insertedData)}\n\nFull data:\n${JSON.stringify(
+					fakeDataDirect
+				)}`
+			);
+		}
+
+		return new Response('Sorry, invalid request');
 	},
 } satisfies ExportedHandler<Env>;
